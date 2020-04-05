061ba82294c3b40ab632d1de3410d1f5868561e0
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2020-04-05 22:56:57 +0700

    Lession 21: GraphQL NotNull

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,7 +9,8 @@ const {
     GraphQLSchema,
     GraphQLID,
     GraphQLInt,
-    GraphQLList
+    GraphQLList,
+    GraphQLNonNull
 } = graphql;
 
 // dummy data
@@ -106,8 +107,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor:{
             type: AuthorType,
             args: {
-                name: {type: GraphQLString},
-                age: {type: GraphQLInt}
+                name: {type: GraphQLNonNull(GraphQLString)},
+                age: {type: GraphQLNonNull(GraphQLInt)}
             },
             resolve(parent, args){
                 let author = new Author({
@@ -120,9 +121,9 @@ const Mutation = new GraphQLObjectType({
         addBook:{
             type: BookType,
             args:{
-                name: {type: GraphQLString},
-                genre: {type: GraphQLString},
-                authorId: {type: GraphQLID}
+                name: {type: GraphQLNonNull(GraphQLString)},
+                genre: {type: GraphQLNonNull(GraphQLString)},
+                authorId: {type: GraphQLNonNull(GraphQLID)}
             },
             resolve(parent, args){
                 let book = new Book({
