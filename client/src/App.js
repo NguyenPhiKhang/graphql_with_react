8ad7a17125a9df71d46026a813f9474ab83456f5
NguyenPhiKhang <17520616@gm.uit.edu.vn>
2020-05-04 13:16:56 +0700

    update

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import BookList from './components/BookList';
 import AddBook from './components/AddBook';
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  uri: "https://gql-uit.herokuapp.com/graphql"
 })
 
 class App extends Component {
